99f4b9bc3b0442e5a65044df44c7f9961021f3fb
akashgaikwad26 <gaikwadak172@gmail.com>
2025-05-26 11:18:54 +0530

    added

---
+++ SimpleCSharpApp/MathOperations.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class MathOperations
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
---
+++ SimpleCSharpApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathOperations mathOps = new MathOperations();
+        int sum = mathOps.Add(5, 3);
+        int difference = mathOps.Subtract(10, 4);
+        
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Difference: {difference}");
+    }
+}
